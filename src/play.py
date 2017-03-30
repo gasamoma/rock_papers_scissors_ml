75fc227573124ec7ec7352da30af0b3e25094c5b
Gabriel <gabriel.montero@mocreditomovil.com>
2017-03-30 13:47:18 -0500

    forking scissors

--- src/play.py
+++ src/play.py
@@ -1,6 +1,14 @@
 import random
-choices = {'r':'rock', 'p': 'paper', 's':'scissors'}
+
+choices = {'r': 'rock', 'p': 'paper', 's': 'scissors'}
 rules = {'r': ['s'], 's': ['p'], 'p': ['r']}
+deviation0 = 2
+deviation1 = 5
+deviation2 = 10
+deviation3 = 15
+
+
+def think(historical):
 
 
 def play():
@@ -10,18 +18,21 @@ def play():
     pc_wins = 0
     user_wins = 0
     ties = 0
+    historical = []
     while True:
         user_play = raw_input("Play: ")
+        thinked_play = think(historical)
         pc_play = random.choice(choices.keys())
         if user_play in choices:
+            historical.append(user_play)
             if pc_play == user_play:
-                ties +=1
+                ties += 1
                 print "Tie"
             elif pc_play in rules[user_play]:
-                user_wins +=1
+                user_wins += 1
                 print "User Wins"
             elif user_play in rules[pc_play]:
-                pc_wins +=1
+                pc_wins += 1
                 print "Pc Wins"
             else:
                 print "wierd"
@@ -29,13 +40,15 @@ def play():
             break
         else:
             print "Not a Valid Option"
-    total = user_wins+pc_wins+ties
+    total = user_wins + pc_wins + ties
     pc_w_r = (pc_wins * 100 / total)
     us_w_r = (user_wins * 100 / total)
     ties_w_r = (ties * 100 / total)
     print "Stats"
     print "Total games:", total
     print "Pc Win rate: ", pc_w_r, "%"
-    print "User Win rate: ", us_w_r , "%"
+    print "User Win rate: ", us_w_r, "%"
     print "Ties Rate: ", ties_w_r, "%"
+
+
 play()
