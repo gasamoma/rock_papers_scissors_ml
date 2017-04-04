092b9e588120c783dd4c687889be1431e8624fad
Gabriel <gabriel.montero@mocreditomovil.com>
2017-04-04 13:39:15 -0500

    Basic User Learning with 2 deviations

--- readme.md
+++ readme.md
@@ -1,2 +1,9 @@
 # Rock paper and scissors
-This code its a simple rock papers a scissors game, that shows you stats of pc User and ties
+User Learning Code
+This code its a machine learning rock papers a scissors game, that shows you stats of pc User and ties
+it bases on two deviations 2 and 6, it tries to calculate 2 different trends of the user. it returns the value with
+the lower deviation first.
+
+It is ready to handle bigger king of rules and choices, since there is a dictionary of rules and options that must
+correlate
+
--- src/play.py
+++ src/play.py
@@ -1,27 +1,77 @@
 import random
-choices = {'r':'rock', 'p': 'paper', 's':'scissors'}
+import math
+choices = {'r': 'rock', 'p': 'paper', 's': 'scissors'}
+reap_choices = dict(choices)
 rules = {'r': ['s'], 's': ['p'], 'p': ['r']}
+deviation1 = 2
+deviation2 = 6
+inverse_rules = dict()
+
+
+def think(historical):
+    n = len(historical)
+    if n > deviation1:
+        subset = historical[-deviation1:n-1]
+        max_l, max_n = most_played(subset)
+        if max_l != '':
+            return inverse_rules[max_l][0]
+    if n > deviation2:
+        subset = historical[-deviation2:n-1]
+        max_l, max_n = most_played(subset)
+        if max_l != '':
+            return inverse_rules[max_l][0]
+    return random.choice(choices.keys())
+
+
+def most_played(set):
+    maxl = ''
+    maxn = 0
+    for key, value in reap_choices.iteritems():
+        reap_choices[key] = 0
+    for x in set:
+        reap_choices[x] += 1
+        if reap_choices[x] > maxn:
+            maxn = reap_choices[x]
+            maxl = x
+    if maxn >= math.ceil(len(set)*0.7):
+        return maxl, maxn
+    else:
+        return '', 0
+
+
+def initialize():
+    global inverse_rules
+    for key, value in rules.iteritems():
+        for x in value:
+            if x in inverse_rules:
+                inverse_rules[x].append(key)
+            else:
+                inverse_rules[x] = [key]
 
 
 def play():
     print "choices ", choices
     print "rules ", rules
     print "type exit to finish game"
+    initialize()
     pc_wins = 0
     user_wins = 0
     ties = 0
+    historical = []
     while True:
         user_play = raw_input("Play: ")
-        pc_play = random.choice(choices.keys())
+        pc_play = think(historical)
+        print "Pc Play ", pc_play
         if user_play in choices:
+            historical.append(user_play)
             if pc_play == user_play:
-                ties +=1
+                ties += 1
                 print "Tie"
             elif pc_play in rules[user_play]:
-                user_wins +=1
+                user_wins += 1
                 print "User Wins"
             elif user_play in rules[pc_play]:
-                pc_wins +=1
+                pc_wins += 1
                 print "Pc Wins"
             else:
                 print "wierd"
@@ -29,6 +79,7 @@ def play():
             break
         else:
             print "Not a Valid Option"
+        print "\n"
     total = user_wins+pc_wins+ties
     pc_w_r = (pc_wins * 100 / total)
     us_w_r = (user_wins * 100 / total)
